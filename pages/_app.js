6260a1af135ae5c4d2f9f924803f377e74a3ee93
sijon Biswas <freefiar99@gmail.com>
2023-03-21 20:46:05 +0530

    moding

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '@/styles/globals.css'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import ScrollToTop from "@/components/ScrollToTop";
+import { Providers } from './upercomponents/providers'
+// import "node_modules/react-modal-video/css/modal-video.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Providers>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+          <ScrollToTop />
+        </Providers>
+  </>
 }
+
